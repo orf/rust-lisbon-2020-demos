aca5d61045e21bc55a13cf673a24f99c22ff6c6a
Tom Forbes <tom@tomforb.es>
2020-03-01 12:18:39 +0000

    Demo #1 - Hello World

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use wasm_bindgen::prelude::*;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let p : web_sys::Node = document.create_element("p")?.into();
+    p.set_text_content(Some("Hello from Rust, WebAssembly, and Webpack!"));
+
+    body.append_child(&p)?;
+
+    Ok(())
+}
