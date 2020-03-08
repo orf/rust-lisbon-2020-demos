32db8df6ab6a757e91a5aa3f2c16721fb6e488a8
Tom Forbes <tom@tomforb.es>
2020-03-08 15:01:57 +0000

    Add comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,13 @@ use wasm_bindgen::prelude::*;
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
-
+    // window.document.body:
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
     let p: web_sys::Node = document.create_element("p")?.into();
-    p.set_text_content(Some("Hello from Rust, WebAssembly, and Webpack!"));
+    p.set_text_content(Some("Hello Lisbon from Rust, WebAssembly, and Webpack!"));
 
     body.append_child(&p)?;
 
