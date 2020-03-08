06afec0f3d4fc53ebcdcc8dc5243d4100d26e803
Tom Forbes <tom@tomforb.es>
2020-03-08 15:35:55 +0000

    Formatting, finish stuff.

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,10 +1,9 @@
-use seed::browser::service::fetch::FailReason;
 use seed::{prelude::*, *};
 use shared::RandomResponse;
 
 #[derive(Default)]
 struct Model {
-    response: Option<seed::fetch::ResponseDataResult<RandomResponse>>,
+    response: Option<RandomResponse>,
 }
 
 #[derive(Clone)]
@@ -13,9 +12,19 @@ enum Msg {
     DataFetched(seed::fetch::ResponseDataResult<RandomResponse>),
 }
 
-async fn fetch_data() -> Result<Msg, Msg> {
-    let url = "http://localhost:8090/random";
-    Request::new(url).fetch_json_data(Msg::DataFetched).await
+fn view(model: &Model) -> impl View<Msg> {
+    let fetch_result = match &model.response {
+        None => div!["Press the button to generate a random number."],
+        Some(response) => div![format!("Random Number: {}", response.number)],
+    };
+    div![
+        h1!["Secure Random Number Generator"],
+        fetch_result,
+        button![
+            simple_ev(Ev::Click, Msg::StartFetching),
+            "Generate Random Number"
+        ]
+    ]
 }
 
 fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
@@ -23,29 +32,18 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
         Msg::StartFetching => {
             orders.perform_cmd(fetch_data());
         }
-        Msg::DataFetched(Ok(res)) => {
-            model.response = Some(Ok(res));
+        Msg::DataFetched(Ok(result)) => {
+            model.response = Some(result);
         }
-        Msg::DataFetched(Err(e)) => {
-            model.response = Some(Err(e));
+        Msg::DataFetched(Err(_)) => {
+            model.response = None;
         }
     }
 }
 
-fn view(model: &Model) -> impl View<Msg> {
-    let result = match &model.response {
-        Some(Ok(e)) => div![format!("Random Number: {}", e.number)],
-        Some(Err(e)) => div![format!("Error fetching: {:?}", e)],
-        None => div!["Press the button to generate a random number."],
-    };
-    div![
-        h1!["Secure Random Number Generator"],
-        result,
-        div![button![
-            simple_ev(Ev::Click, Msg::StartFetching),
-            "Generate Random Number"
-        ],],
-    ]
+async fn fetch_data() -> Result<Msg, Msg> {
+    let url = "http://localhost:8090/random";
+    Request::new(url).fetch_json_data(Msg::DataFetched).await
 }
 
 #[wasm_bindgen(start)]
